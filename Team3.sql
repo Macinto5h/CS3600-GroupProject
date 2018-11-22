4d608583d55032191eea87f689196cbf6f61f369
Macinto5h <44030647+Macinto5h@users.noreply.github.com>
2018-11-22 11:10:32 -0500

    Add files via upload

--- Team3.sql
+++ Team3.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.14  Distrib 5.5.56-MariaDB, for Linux (x86_64)
+-- MySQL dump 10.14  Distrib 5.5.60-MariaDB, for Linux (x86_64)
 --
 -- Host: localhost    Database: Team3
 -- ------------------------------------------------------
--- Server version       5.5.56-MariaDB
+-- Server version	5.5.60-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -91,6 +91,34 @@ INSERT INTO `Employee` VALUES (1,7.55,'Stocker'),(2,8.95,'Cashier'),(3,12,'Store
 UNLOCK TABLES;
 
 --
+-- Table structure for table `IsReturn`
+--
+
+DROP TABLE IF EXISTS `IsReturn`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `IsReturn` (
+  `SalesId` int(10) NOT NULL,
+  `ProductId` int(10) NOT NULL,
+  `Quantity` int(10) DEFAULT NULL,
+  PRIMARY KEY (`SalesId`),
+  KEY `FKIRP` (`ProductId`),
+  CONSTRAINT `FKIR` FOREIGN KEY (`SalesId`) REFERENCES `Sales` (`SalesId`),
+  CONSTRAINT `FKIRP` FOREIGN KEY (`ProductId`) REFERENCES `Product` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `IsReturn`
+--
+
+LOCK TABLES `IsReturn` WRITE;
+/*!40000 ALTER TABLE `IsReturn` DISABLE KEYS */;
+INSERT INTO `IsReturn` VALUES (3,10,1),(5,11,2);
+/*!40000 ALTER TABLE `IsReturn` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `Person`
 --
 
@@ -129,9 +157,11 @@ CREATE TABLE `Product` (
   `Id` int(10) NOT NULL,
   `Price` float NOT NULL,
   `Name` char(50) DEFAULT NULL,
+  `Artist` varchar(20) NOT NULL,
   `Stock` int(10) DEFAULT NULL,
   `ReleaseDate` date DEFAULT NULL,
-  PRIMARY KEY (`Id`)
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Secondary` (`Name`,`Artist`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -141,9 +171,23 @@ CREATE TABLE `Product` (
 
 LOCK TABLES `Product` WRITE;
 /*!40000 ALTER TABLE `Product` DISABLE KEYS */;
-INSERT INTO `Product` VALUES (1,8.99,'Ye - Kanye West, CD',16,'2018-06-01'),(2,4.99,'DropTopWop - Gucci Mane, CD',3,'2016-06-22'),(3,8.99,'Yeezus - Kanye West, CD',6,'2013-06-18'),(4,11.99,'Yandhi - Kanye West, CD',40,'2018-11-23'),(5,5.99,'To Pimp A Butterfly - Kendrick Lamar, CD',12,'2018-04-15'),(6,6.99,'Painting Pictures - Kodak Black, CD',16,'2017-03-31'),(7,6.99,'DAMN. - Kendrick Lamar, CD',13,'2017-06-14'),(8,12.99,'Tha Carter V - Lil Wayne, CD',28,'2018-09-28'),(9,12.99,'The Wack Album - The Lonely Island, Vinyl',3,'2013-06-07'),(10,8.99,'Swimming - Mac Miller, CD',17,'2018-08-03'),(11,6.99,'Culture - Migos, CD',4,'2017-01-27'),(12,6.99,'Stoney - Post Malone, CD',2,'2016-12-09'),(13,7.99,'DAYTONA - Pusha T, CD',6,'2018-05-25'),(14,31.99,'Bad - Michael Jackson, Vinyl',2,'1987-09-01'),(15,4.99,'Blank Face LP - Schoolboy Q, CD',9,'2016-07-08'),(16,3.99,'Deadstar - Smokepurpp, CD',3,'2017-09-29'),(17,9.99,'ASTROWORLD - Travis Scott, CD',21,'2018-08-03'),(18,10.99,'Flower Boy - Tyler The Creator, CD',12,'2017-07-21'),(19,5.99,'Big Fish Theory - Vince Staples, CD',8,'2017-06-23'),(20,15.99,'Slime Season 3 - Young Thug, Vinyl',5,'2016-03-25'),(21,3.99,'Astro Lounge - Smashmouth, CD',15,'1999-06-19');
+INSERT INTO `Product` VALUES (1,8.99,'Ye, CD','Kanye West',16,'2018-06-01'),(2,4.99,'DropTopWop, CD','Gucci Mane',3,'2016-06-22'),(3,8.99,'Yeezus, CD','Kanye West',6,'2013-06-18'),(4,11.99,'Yandhi, CD','Kanye West',40,'2018-11-23'),(5,5.99,'To Pimp A Butterfly, CD','Kendrick Lamar',12,'2018-04-15'),(6,6.99,'Painting Pictures, CD','Kodak Black',16,'2017-03-31'),(7,6.99,'DAMN. CD','Kendrick Lamar',13,'2017-06-14'),(8,12.99,'Tha Carter V, CD','Lil Wayne',28,'2018-09-28'),(9,12.99,'The Wack Album, Vinyl','The Lonely Island',3,'2013-06-07'),(10,8.99,'Swimming, CD','Mac Miller',17,'2018-08-03'),(11,6.99,'Culture, CD','Migos',4,'2017-01-27'),(12,6.99,'Stoney, CD','Post Malone',2,'2016-12-09'),(13,7.99,'DAYTONA, CD','Pusha T',6,'2018-05-25'),(14,31.99,'Bad, Vinyl','Michael Jackson',2,'1987-09-01'),(15,4.99,'Blank Face LP, CD','Schoolboy Q',9,'2016-07-08'),(16,3.99,'Deadstar, CD','Smokepurpp',3,'2017-09-29'),(17,9.99,'ASTROWORLD, CD','Travis Scott',21,'2018-08-03'),(18,10.99,'Flower Boy, CD','Tyler, The Creator',12,'2017-07-21'),(19,5.99,'Big Fish Theory, CD','Vince Staples',8,'2017-06-23'),(20,15.99,'Slime Season 3, Vinyl','Young Thug',5,'2016-03-25'),(21,3.99,'Astro Lounge, CD','Smashmouth',15,'1999-06-19');
 /*!40000 ALTER TABLE `Product` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Temporary table structure for view `ProductSold`
+--
+
+DROP TABLE IF EXISTS `ProductSold`;
+/*!50001 DROP VIEW IF EXISTS `ProductSold`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `ProductSold` (
+  `Id` tinyint NOT NULL,
+  `QuantitySold` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `Sales`
@@ -155,16 +199,10 @@ DROP TABLE IF EXISTS `Sales`;
 CREATE TABLE `Sales` (
   `SalesId` int(10) NOT NULL,
   `CustomerId` int(10) NOT NULL,
-  `ProductId` int(10) NOT NULL,
   `CurrentDate` date DEFAULT NULL,
-  `Quantity` int(10) DEFAULT NULL,
-  `IsReturn` tinyint(1) DEFAULT NULL,
-  `ReturnDate` date DEFAULT NULL,
-  PRIMARY KEY (`SalesId`,`ProductId`),
-  KEY `FKCID` (`CustomerId`),
-  KEY `FKPID` (`ProductId`),
-  CONSTRAINT `FKCID` FOREIGN KEY (`CustomerId`) REFERENCES `Customer` (`Id`),
-  CONSTRAINT `FKPID` FOREIGN KEY (`ProductId`) REFERENCES `Product` (`Id`)
+  PRIMARY KEY (`SalesId`) USING BTREE,
+  KEY `FKCID` (`CustomerId`) USING BTREE,
+  CONSTRAINT `FKCID` FOREIGN KEY (`CustomerId`) REFERENCES `Customer` (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -174,8 +212,35 @@ CREATE TABLE `Sales` (
 
 LOCK TABLES `Sales` WRITE;
 /*!40000 ALTER TABLE `Sales` DISABLE KEYS */;
-INSERT INTO `Sales` VALUES (1,21,14,'2018-10-30',1,0,NULL),(2,9,3,'2018-10-30',3,0,NULL),(3,19,10,'2018-10-30',1,1,'2018-11-01'),(3,19,12,'2018-10-30',1,0,NULL),(4,20,16,'2018-10-31',1,0,NULL),(5,8,11,'2018-10-31',2,1,'2018-11-01'),(6,18,21,'2018-10-31',10,0,NULL),(7,17,7,'2018-10-31',3,0,NULL),(8,14,14,'2018-11-01',1,0,NULL),(9,9,20,'2018-11-01',1,0,NULL),(10,16,5,'2018-11-01',1,0,NULL);
+INSERT INTO `Sales` VALUES (1,21,'2018-10-30'),(2,9,'2018-10-30'),(3,19,'2018-10-30'),(4,20,'2018-10-31'),(5,8,'2018-10-31'),(6,18,'2018-10-31'),(7,17,'2018-10-31'),(8,14,'2018-11-01'),(9,9,'2018-11-01'),(10,16,'2018-11-01');
 /*!40000 ALTER TABLE `Sales` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `SalesItem`
+--
+
+DROP TABLE IF EXISTS `SalesItem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `SalesItem` (
+  `SalesId` int(10) NOT NULL,
+  `ProductId` int(10) NOT NULL DEFAULT '0',
+  `Quantity` int(10) NOT NULL,
+  PRIMARY KEY (`SalesId`,`ProductId`) USING BTREE,
+  KEY `FKCI` (`Quantity`),
+  CONSTRAINT `FKSI` FOREIGN KEY (`SalesId`) REFERENCES `Sales` (`SalesId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `SalesItem`
+--
+
+LOCK TABLES `SalesItem` WRITE;
+/*!40000 ALTER TABLE `SalesItem` DISABLE KEYS */;
+INSERT INTO `SalesItem` VALUES (1,14,1),(3,12,1),(4,16,1),(8,14,1),(9,20,1),(10,5,1),(2,3,3),(7,7,3),(6,21,10);
+/*!40000 ALTER TABLE `SalesItem` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -205,6 +270,25 @@ LOCK TABLES `WorksIn` WRITE;
 INSERT INTO `WorksIn` VALUES (1,'2018-10-30',7),(2,'2017-05-11',7),(3,'2017-05-12',6),(4,'2017-05-16',2),(5,'2017-08-31',6),(6,'2018-07-22',4),(10,'2017-02-21',3),(11,'2017-03-11',10),(12,'2016-11-29',3),(13,'2017-01-15',7),(15,'2017-03-03',5);
 /*!40000 ALTER TABLE `WorksIn` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Final view structure for view `ProductSold`
+--
+
+/*!50001 DROP TABLE IF EXISTS `ProductSold`*/;
+/*!50001 DROP VIEW IF EXISTS `ProductSold`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`prs1005`@`%` SQL SECURITY DEFINER */
+/*!50001 VIEW `ProductSold` AS select `P`.`Id` AS `Id`,sum(`I`.`Quantity`) AS `QuantitySold` from (`Product` `P` join `SalesItem` `I`) where (`P`.`Id` = `I`.`ProductId`) group by `P`.`Id` */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -215,4 +299,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-30 15:42:51
+-- Dump completed on 2018-11-22 11:08:08
