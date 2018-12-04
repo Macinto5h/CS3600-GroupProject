3a44f0bb09aecefc842b124c0f74a2e7093885b2
Macinto5h <44030647+Macinto5h@users.noreply.github.com>
2018-12-03 19:16:45 -0500

    Add files via upload

---
+++ customerRegistration.php
@@ -0,0 +1,55 @@
+<!--This is the php file that needs to be modified and used for data entry -mac -->
+<!Doctype html>
+<html>
+  <head>
+    <title>Database Connection</title>
+    <link rel="stylesheet" href="primaryStyle.css" />
+  </head>
+    <body>
+      <?php
+        //Set values for local host, user, and password
+        include("phpbook-vars.inc");
+
+        //Connect to a Database
+        $database="Team3";
+
+        $connect=mysqli_connect($hostname,$user,$password);
+        //Check connection
+        //mysqli_connect_errno() return 0, false, if nothing goes wrong
+        if (!$connect)
+          echo "<b>Error occurred, failed to connect with MySQL:".
+          mysqli_connect_error()."</b>";
+        //Select the data from the database
+        if (!mysqli_select_db($connect,$database))
+          echo "Database $database not selected";
+        $firstName=$_POST['firstName'];
+        $lastName=$_POST['lastName'];
+        $phoneNumber=$_POST['phoneNumber'];
+        $email=$_POST['email'];
+        $address=$_POST['address'];
+        $receivesMail=$_POST['receivesMail'];
+
+        //Notice that we have to use single quotes to enclose the variables. --zs
+        $query="INSERT INTO Person (firstName, lastName, phoneNum, email,
+        address) Values ('$firstName','$lastName','$phoneNumber','$email',
+        '$address')";
+        if(!mysqli_query($connect,$query))
+          echo "Failed to Insert into person table";
+        else echo "Successful insertion into person table<br>";
+        $query="SELECT Id From Person Where firstName='$firstName' and
+        lastName='$lastName' and phoneNum='$phoneNumber' and email='$email' AND
+        address='$address'";
+        $result = mysqli_query($connect,$query);
+        while($row=$result->fetch_assoc()) {
+          $Id = $row['Id'];
+        }
+        $query="INSERT INTO Customer (Id,receivesMail) Values ('$Id','$receivesMail')";
+        if(!mysqli_query($connect,$query))
+          echo "Failed to Insert into customer table";
+        else echo "Successful insertion into customer table";
+
+        //Refresh the form after three seconds. --zs
+        header ("refresh:3; url=DatabaseEnterData.html");
+      ?>
+    </body>
+  </html>
---
+++ saleRegistration.php
@@ -0,0 +1,52 @@
+<!--This is the php file that needs to be modified and used for data entry -mac -->
+<!Doctype html>
+<html>
+  <head>
+    <title> Database Connection</title>
+    <style>
+      body{
+        background: :-webkit-linear-gradient (left, lightgreen, yellow);
+      }
+      Form{
+        border: solid;
+        background: lightyellow;
+        width: 400px;
+        padding: 20px;
+      }
+      h1{color: green;}
+    </style>
+    <body>
+      <?php
+        //Set values for local host, user, and password
+        include("phpbook-vars.inc");
+
+        //Connect to a Database
+        $database="Team3";
+
+        $connect=mysqli_connect($hostname,$user,$password);
+        //Check connection
+        //mysqli_connect_errno() return 0, false, if nothing goes wrong
+        if (!$connect)
+          echo "<b>Error occurred, failed to connect with MySQL:".
+          mysqli_connect_error()."</b>";
+        //Select the data from the database
+        if (!mysqli_select_db($connect,$database))
+          echo "Database $database not selected";
+        $education=$_POST['education'];
+        $country=$_POST['country'];
+
+        //Notice that we have to use single quotes to enclose the variables. --zs
+        $query="INSERT INTO userdata (name, email, password, gender,
+        contact, education, country) Values ('$name','$email','$passwd','$gender',
+        '$contact','$education','$country')";
+
+        //Check to see if it is executed successfully. --zs
+        if(!mysqli_query($connect,$query))
+          echo "It is not inserted";
+        else echo "<h1>Thanks for your registration!</h1>";
+
+        //Refresh the form after three seconds. --zs
+        header ("refresh:3; url=enterData.html");
+      ?>
+    </body>
+  </html>
